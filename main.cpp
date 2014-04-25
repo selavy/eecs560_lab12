eb89e6894f7fcb4cd19a2c8fc6eaf6c51b544ffb
Peter Lesslie <pclesslie@gmail.com>
2014-04-25 12:41:06 -0500

    Generate full adjacency lists

--- main.cpp
+++ main.cpp
@@ -8,54 +8,32 @@
 
 using namespace std;
 
-void solve( vector< vector<int> >& graph ) {
-  size_t counter = 1;
-  queue<int> Q;
-  vector<bool> visited; // maybe use bitset?
+void solve( vector<vector<int> >& graph ) {
   graph.push_back( vector<int>(0) );
-  vector<int> lows;
-  lows.reserve( graph.size() );
-  visited.reserve( graph.size() );
-  for( size_t i = 0; i < graph.size(); ++i ) {
-    visited.push_back( false );
-    lows.push_back( 0 );
-  }
+  vector<vector<int> > full_graph;
+  full_graph.reserve( graph.size() );
 
-  Q.push( 1 );
-  cout << "Graph size = " << static_cast<int>( graph.size() ) << endl;
-  cout << "Visited size = " << static_cast<int>( visited.size() ) << endl;
+  for( size_t v = 0; v < graph.size(); ++v ) {
+    auto row = graph.at(v);
+    vector<int> adj_list;
+    for_each( row.begin(), row.end(), [&](int n) { if( n != 0 ) { adj_list.push_back(n); } } );
 
-  while(! Q.empty() ) {
-    int v = Q.front();
-    Q.pop();
-    if( v == 0 ) continue;
-    --v;
-    if( visited.at(v) ) continue;
-    visited.at(v) = true;
-    lows.at(v) = counter++;
-
-    int j = 0;
-    for( auto it = graph.begin(); (it != graph.end()) && (j <= v); ++j, ++it ) {
-      if( j == v ) {
-	auto& it = graph.at(v);
-	for_each( it.begin(), it.end(), [&](int i) { 
-	    if( (i > 0) && !visited.at(i-1) ) {
-	      Q.push(i);
-	    }
-	  });
-      }
-      else if( any_of( it->begin(), it->end(), [&](int i) { return(i == (v+1)); } ) ) {
-	if(! visited.at(j) ) {
-	  Q.push(j+1);
-	}
+    for( int i = 0; i < static_cast<int>(v); ++i ) {
+      row = graph.at(i);
+      if( any_of( row.begin(), row.end(), [&](int n) { return (n == static_cast<int>(v+1)); } ) ) {
+	adj_list.push_back(i+1);
       }
     }
+    
+    sort( adj_list.begin(), adj_list.end() );
+    full_graph.push_back( adj_list );
   }
 
-  int i = 1;
-  for( auto& it : lows ) {
-    cout << i << " : " << it << endl;
-    ++i;
+  for( auto it : full_graph ) {
+    for( auto itt : it ) {
+      cout << itt << " ";
+    }
+    cout << endl;
   }
 }
 
